6ff97d7e62103558b0b4a30bba61a4eecfe8bb3a
wiltrahan <twilorip@gmail.com>
2018-08-10 17:52:06 -0400

    test for neg numbers

--- src/tdd_practice/code/StringCalculator.java
+++ src/tdd_practice/code/StringCalculator.java
@@ -2,18 +2,23 @@ package tdd_practice.code;
 
 public class StringCalculator {
 
-	public int add(String numbers) {
+	public int add(String numbers) throws Exception {
 		if(numbers.isEmpty()) {
 			return 0;	
 		} 	
 		return splitAndAddString(numbers);
 	}
 	
-	public int splitAndAddString(String numString) {
+	public int splitAndAddString(String numbers) throws Exception {
 		int total = 0;
-		String[] numArray = numString.split("[,\n]");
+		String[] numArray = numbers.split("[,\n]");
 		
 		for(int i = 0; i < numArray.length; i++) {
+			
+			if(Integer.parseInt(numArray[i]) < 0) {
+				throw new Exception("negatives not allowed");
+			}
+			
 			total += Integer.parseInt(numArray[i]);
 		}
 		return total;
--- src/tdd_practice/tests/StringCalculatorTest.java
+++ src/tdd_practice/tests/StringCalculatorTest.java
@@ -3,7 +3,9 @@ package tdd_practice.tests;
 import static org.junit.Assert.*;
 
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import tdd_practice.code.StringCalculator;
 
@@ -11,38 +13,52 @@ public class StringCalculatorTest {
 	
 	private static StringCalculator _stringCalc;
 	
+	
+	
 	@BeforeClass
 	public static void setUpStringCalculator() {
 		_stringCalc = new StringCalculator();
 	}
 
 	@Test
-	public void testForEmptyString() {
+	public void testForEmptyString() throws Exception {
 		assertEquals(_stringCalc.add(""), 0);
 	}
 	
 	@Test
-	public void testForStringOfOne() {
+	public void testForStringOfOne() throws Exception {
 		assertEquals(_stringCalc.add("2"), 2);
 	}
 	
 	@Test
-	public void testForStringOfTwo() {
+	public void testForStringOfTwo() throws Exception {
 		assertEquals(_stringCalc.add("2,3"), 5);
 		assertEquals(_stringCalc.add("4,5"), 9);
 		assertEquals(_stringCalc.add("9,3"), 12);
 	}
 	
 	@Test
-	public void testForStringLongerThanTwo() {
+	public void testForStringLongerThanTwo() throws Exception {
 		assertEquals(_stringCalc.add("2,3,4"), 9);
 		assertEquals(_stringCalc.add("5,10,10"), 25);
 		assertEquals(_stringCalc.add("5,10,10,30"), 55);
 	}
 	
 	@Test
-	public void testForStringWithNewLine() {
+	public void testForStringWithNewLine() throws Exception {
 		assertEquals(_stringCalc.add("1\n2,3"), 6);
+	}
+	
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
+	@Test
+	public void testForExceptionThrownDueToNegativeNumber() throws Exception {
+		
+		//thrown.expect(Exception.class);
+		thrown.expectMessage("negatives not allowed");
+		
+		_stringCalc.add("2,-5,99");
 	}
 	
 }
